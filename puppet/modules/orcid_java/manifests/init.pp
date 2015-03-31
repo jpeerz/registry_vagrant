694523e70b1c07fbf646f75a7e91f1d5614125b9
rcpeters <info@rcpeters.com>
2015-03-31 00:28:23 -0700

    install java

--- puppet/modules/orcid_java/manifests/init.pp
+++ puppet/modules/orcid_java/manifests/init.pp
@@ -1,11 +1,12 @@
 class orcid_java  () {
 
-  exec { "java install":
-    environment => [ "DEBIAN_FRONTEND=noninteractive" ], # same as export DEBIAN_FRONTEND=noninteractive
-    command => template("orcid_java/scripts/install_java.erb"),
-    creates => "/usr/bin/java",
-    require => Exec["apt-get update"]
-  }
+   exec { "java install":
+      environment => [ "DEBIAN_FRONTEND=noninteractive" ], # same as export DEBIAN_FRONTEND=noninteractive
+      command => template("orcid_java/scripts/install_java.erb"),
+      creates => "/usr/bin/java",
+      returns => [0, 1],
+      require => Exec["apt-get update"],
+   }
 
 }
 
