709dc7097b2c79e8e35b7636e30326b932b5fcfa
Will Simpson <will@wjrs.co.uk>
2015-07-22 18:56:55 +0100

    Whitespace tidy up.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -18,14 +18,14 @@ user { "orcid_tomcat":
 class { 'orcid_java': }
 
 class {
-   orcid_tomcat:
-   tomcat_catalina_opts => '-Xmx2000m -XX:MaxPermSize=512m
+  orcid_tomcat:
+  tomcat_catalina_opts => '-Xmx2000m -XX:MaxPermSize=512m
 -Dfile.encoding=utf-8 
 -Dorg.orcid.config.file=file:///home/orcid_tomcat/git/ORCID-Source/orcid-persistence/src/main/resources/staging-persistence.properties 
 -Dsolr.solr.home=/home/orcid_tomcat/bin/tomcat/webapps/orcid-solr-web/solr 
 -Dsolr.data.dir=/home/orcid_tomcat/data/orcid-solr 
 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true
 ',
-     require => [Class["orcid_java"],User["orcid_tomcat"]]
+  require => [Class["orcid_java"],User["orcid_tomcat"]]
 }
 
