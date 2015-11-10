ce1ee54c664b75fa5e42f06d8beff88054327e63
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-11-10 17:20:31 -0600

    Using an existing module to install jenkins

--- puppet/modules/orcid_jenkins/manifests/init.pp
+++ puppet/modules/orcid_jenkins/manifests/init.pp
@@ -1,14 +1,10 @@
 class orcid_jenkins {
-
-  jenkins::plugin { 'git': }
-  jenkins::plugin { 'git-client': }
-  jenkins::plugin { 'github-api': }
-
-  file { "/var/lib/jenkins":
-        ensure  => directory,
-        owner => orcid_tomcat,
-        group => orcid_tomcat,
-  }
+	
+	include jenkins
+	
+	jenkins::plugin { 'git': }
+	jenkins::plugin { 'git-client': }
+	jenkins::plugin { 'github-api': }  
 
   file { "/var/cache/jenkins":
         ensure  => directory,
@@ -20,30 +16,7 @@ class orcid_jenkins {
         ensure  => directory,
         owner => orcid_tomcat,
         group => orcid_tomcat,
-  }      
-
-  file { "/var/lib/jenkins/plugins":
-  	ensure	=> directory,
-	owner => orcid_tomcat,
-	group => orcid_tomcat,
-	recurse => true,
-  }
-
-  file { "/var/lib/jenkins/plugins/git-client.hpi":
-  	ensure => file,
-	owner => orcid_tomcat,
-	group => orcid_tomcat, 
-  }
-
-  package { "jenkins":
-        ensure => installed,
-  }
-
-  service { "jenkins":
-    ensure    => running,
-    enable    => true,
-    require   => Package["jenkins"],
-  }
+  }  
 
   $jenkins_ext_fw = 'server custom tomcat tcp/8383 default accept'
 
@@ -56,7 +29,7 @@ class orcid_jenkins {
         ensure        => file,
         path          => '/etc/default/jenkins',
         content       => template('orcid_jenkins/etc/default/jenkins.erb'),
-  }    
+  }          
 }
 
 
