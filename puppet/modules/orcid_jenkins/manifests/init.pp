3e36d325215142b3d066ff29c5b77e4288df30e4
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-11-11 14:56:35 -0600

    Still dont work with the orcid_tomcat user

--- puppet/modules/orcid_jenkins/manifests/init.pp
+++ puppet/modules/orcid_jenkins/manifests/init.pp
@@ -1,21 +1,26 @@
 class orcid_jenkins {	
 	
-	exec { 'ensure_orcid_tomcat_is_ready':
-		require => User["orcid_tomcat"],
-	}
-
 	class {'jenkins':
-		lts => true,
 		user => 'orcid_tomcat',
 		group => 'orcid_tomcat',
 		manage_user  => false,
-		manage_group => false,
-		install_java => false,		
+		manage_group => false,	
+		install_java => false,
+		service_enable	=> true,
+		service_ensure	=> 'running',
+		config_hash	=> {
+			'HTTP_PORT' => {value => '8383'},
+			'JENKINS_USER' => {value => 'orcid_tomcat'},
+			'JENKINS_GROUP' => {value => 'orcid_tomcat'},
+			'JAVA_HOME' => {value => '/usr/lib/jvm/java-8-oracle/'}
+		},
+		require => User["orcid_tomcat"],
 	}
 	
 	jenkins::plugin { 'git': }
 	jenkins::plugin { 'git-client': }
-	jenkins::plugin { 'github-api': }    
+	jenkins::plugin { 'github-api': }  
+
 }
 
 
