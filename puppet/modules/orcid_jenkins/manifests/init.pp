befd4344b8b534125d89f8bf1c4c4a899bde5215
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-11-11 16:33:51 -0600

    Still cant make it work

--- puppet/modules/orcid_jenkins/manifests/init.pp
+++ puppet/modules/orcid_jenkins/manifests/init.pp
@@ -7,20 +7,37 @@ class orcid_jenkins {
 		manage_group => false,	
 		install_java => false,
 		service_enable	=> true,
-		service_ensure	=> 'running',
+		service_ensure	=> 'stopped',
 		config_hash	=> {
 			'HTTP_PORT' => {value => '8383'},
 			'JENKINS_USER' => {value => 'orcid_tomcat'},
 			'JENKINS_GROUP' => {value => 'orcid_tomcat'},
 			'JAVA_HOME' => {value => '/usr/lib/jvm/java-8-oracle/'}
 		},
-		require => User["orcid_tomcat"],
+		require => User['orcid_tomcat'],
 	}
 	
 	jenkins::plugin { 'git': }
 	jenkins::plugin { 'git-client': }
 	jenkins::plugin { 'github-api': }  
 
+	file { '/usr/share/jenkins/':		
+		owner => orcid_tomcat,
+		group => orcid_tomcat,    
+		recurse => true,
+	}
+	
+	file { '/var/cache/jenkins/':
+		owner => orcid_tomcat,
+		group => orcid_tomcat,    
+		recurse => true,
+	}
+	
+	file { '/var/run/jenkins/':
+		owner => orcid_tomcat,
+		group => orcid_tomcat,    
+		recurse => true,
+	}	
 }
 
 
