c225954719a46138ffe5562a62c59c59f493b40b
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-11-16 08:45:11 -0600

    Just install jenkins and set it to run in port 8383

--- puppet/modules/orcid_jenkins/manifests/init.pp
+++ puppet/modules/orcid_jenkins/manifests/init.pp
@@ -1,43 +1,15 @@
 class orcid_jenkins {	
 	
 	class {'jenkins':
-		user => 'orcid_tomcat',
-		group => 'orcid_tomcat',
-		manage_user  => false,
-		manage_group => false,	
 		install_java => false,
-		service_enable	=> true,
-		service_ensure	=> 'stopped',
 		config_hash	=> {
-			'HTTP_PORT' => {value => '8383'},
-			'JENKINS_USER' => {value => 'orcid_tomcat'},
-			'JENKINS_GROUP' => {value => 'orcid_tomcat'},
-			'JAVA_HOME' => {value => '/usr/lib/jvm/java-8-oracle/'}
+			'HTTP_PORT' => {value => '8383'},			
 		},
-		require => User['orcid_tomcat'],
 	}
 	
 	jenkins::plugin { 'git': }
 	jenkins::plugin { 'git-client': }
-	jenkins::plugin { 'github-api': }  
-
-	file { '/usr/share/jenkins/':		
-		owner => orcid_tomcat,
-		group => orcid_tomcat,    
-		recurse => true,
-	}
-	
-	file { '/var/cache/jenkins/':
-		owner => orcid_tomcat,
-		group => orcid_tomcat,    
-		recurse => true,
-	}
-	
-	file { '/var/run/jenkins/':
-		owner => orcid_tomcat,
-		group => orcid_tomcat,    
-		recurse => true,
-	}	
+	jenkins::plugin { 'github-api': }  	
 }
 
 
