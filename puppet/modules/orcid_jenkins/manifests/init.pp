0980a11f0f0ddb0dcedcc5a7fe04f66999398f9c
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-11-17 09:32:16 -0600

    Use orcid_tomcat to start jenkins

--- puppet/modules/orcid_jenkins/manifests/init.pp
+++ puppet/modules/orcid_jenkins/manifests/init.pp
@@ -1,9 +1,15 @@
 class orcid_jenkins {	
 	
 	class {'jenkins':
+		user => 'orcid_tomcat',
+		group => 'orcid_tomcat',
+		manage_user  => false,
+		manage_group => false,	
 		install_java => false,
 		config_hash	=> {
-			'HTTP_PORT' => {value => '8383'},	
+			'HTTP_PORT' => {value => '8383'},
+			'JENKINS_USER' => {value => 'orcid_tomcat'},
+			'JENKINS_GROUP' => {value => 'orcid_tomcat'},			
 			'JAVA_HOME' => {value => '/usr/lib/jvm/java-8-oracle/'},			
 		},
 	}
