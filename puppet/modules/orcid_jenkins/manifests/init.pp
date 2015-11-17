c46d1f515ede7e513a483bb5b2291acc6877ff9f
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-11-17 13:35:56 -0600

    We will manually install jenkins

--- puppet/modules/orcid_jenkins/manifests/init.pp
+++ puppet/modules/orcid_jenkins/manifests/init.pp
@@ -1,55 +1,25 @@
 class orcid_jenkins {	
-	
-	class {'jenkins':
-		user => 'orcid_tomcat',
-		group => 'orcid_tomcat',
-		manage_user  => false,
-		manage_group => false,	
-		install_java => false,
-		config_hash	=> {
-			'HTTP_PORT' => {value => '8383'},
-			'JENKINS_USER' => {value => 'orcid_tomcat'},
-			'JENKINS_GROUP' => {value => 'orcid_tomcat'},			
-			'JAVA_HOME' => {value => '/usr/lib/jvm/java-8-oracle/'},			
-		},
+
+	exec { 'install_jenkins_package_keys':
+		command => '/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | /usr/bin/apt-key add - ',
 	}
-	
-	jenkins::plugin { 'credentials': 
-		version => '1.22',
+
+	file { "/etc/apt/sources.list.d/jenkins.list":
+		mode => 644,
+		owner => orcid_tomcat,
+		group => orcid_tomcat,
+		source => "puppet:///modules/jenkins/etc/apt/sources.list.d/jenkins.list",
 	}
-	jenkins::plugin { 'ssh-credentials': 
-		version => '1.11',
+
+	package { 'orcid_jenkins':
+		ensure => latest,
+		require  => [ Exec['install_jenkins_package_keys'],
+		File['/etc/apt/sources.list.d/jenkins.list'], ],
 	}
-	jenkins::plugin { 'scm-api': 
-		version => '0.2',
+
+	service { 'jenkins':
+		ensure => running,
 	}
-	jenkins::plugin { 'mailer': 
-		version => '1.15',
-	}
-	jenkins::plugin { 'matrix-project': 
-		version => '1.4',
-	}
-	jenkins::plugin { 'junit': 
-		version => '1.2',
-	}
-	jenkins::plugin { 'script-security': 
-		version => '1.13',
-	}
-	jenkins::plugin { 'git-client': 
-		version => '1.18.0',
-	}
-	jenkins::plugin { 'git': 
-		version => '2.4.0',
-	}	
-	jenkins::plugin { 'plain-credentials': 
-		version => '1.1',
-	}
-	jenkins::plugin { 'github-api': 
-		version => '1.69',
-	} 
-	jenkins::plugin { 'github': 
-		version => '1.14.0',
-	}	 
 }
 
 
