59c7133d8481114e4b20d554f0ea87f623fd2319
rcpeters <info@rcpeters.com>
2016-01-08 15:14:45 -0800

    update python build to include virtualenv

--- puppet/modules/orcid_python/manifests/init.pp
+++ puppet/modules/orcid_python/manifests/init.pp
@@ -1,34 +1,36 @@
 class orcid_python {
 
-  package { "python":
-    ensure => installed,
+
+  exec { "python secure":
+     environment => [ "DEBIAN_FRONTEND=noninteractive" ], # same as export DEBIAN_FRONTEND=noninteractive
+     command => "sudo apt-get -fy remove python; sudo apt-get -fy install python; sudo touch /usr/lib/python2.7/python_ssl.touched",
+     returns => [0, 1],
+     creates => "/usr/lib/python2.7/python_ssl.touched",
+     require =>  Package["libssl-dev"],
   }
 
-  package { "python-setuptools":
-    ensure => installed,
+  $packages = [
+    "libssl-dev", "python", "python-setuptools", "libffi-dev", "python2.7-dev",
+    "python-dev", "libpq-dev", "python-openssl", "libcurl4-openssl-dev", "python-pip" 
+  ]
+
+  # install packages
+  package { $packages:
+    ensure => present,
+    require => Exec["apt-get update"]
   }
 
-  package { "python-pip":
-    ensure => installed,
-  }
-  
   package { "pip":
     ensure => latest,
     provider => pip,
     require =>  Package["python-pip"],
   }
 
-  package { "python-dev":
-    ensure => installed,
-  }
-  
-  package { "python-gnupg":
-    ensure => latest,
-    provider => pip,
-    require =>  Package["pip"],
-  }
+  $pip_packages = [
+    "virtualenv", "python-gnupg", "pycurl", "filechunkio",
+  ]
 
-  package { "filechunkio":
+  package { $pip_packages:
     ensure => latest,
     provider => pip,
     require =>  Package["pip"],
