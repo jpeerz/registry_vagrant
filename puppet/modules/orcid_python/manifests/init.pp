e3688d9bf465ff2b6e00d6e1b2d938150bd78ca6
rcpeters <info@rcpeters.com>
2016-01-19 12:29:45 -0800

    always install latest pyrax

--- puppet/modules/orcid_python/manifests/init.pp
+++ puppet/modules/orcid_python/manifests/init.pp
@@ -27,22 +27,14 @@ class orcid_python {
   }
 
   $pip_packages = [
-    "virtualenv", "python-gnupg", "pycurl", "filechunkio", "requests[security]"
+    "virtualenv", "python-gnupg", "pycurl", "filechunkio", "requests[security]", "certifi",
+    "wrapt", "pyrax", "netifaces", "monotonic", "GitPython"
   ]
 
   package { $pip_packages:
     ensure => latest,
     provider => pip,
     require =>  Package["pip"],
-  }
-
-  exec { 'pyrax install':
-    command => 'sudo pip install --upgrade  wrapt pyrax netifaces monotonic;
-sudo easy_install --upgrade GitPython;
-touch /usr/local/lib/pryax_puppet_installed;
-',
-    creates => '/usr/local/lib/pryax_puppet_installed',
-    require => [Package["pip"]]
   }
 
 }
