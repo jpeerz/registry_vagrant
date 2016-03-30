72618707c3cbc08206d3c99dc7268c9fd39bb053
Liz Krznarich <ejkrznarich@gmail.com>
2016-03-30 18:17:54 -0400

    add orcid_txgh user and update puppet commands to use orcid_txgh home directory

--- puppet/manifests/txgh_default.pp
+++ puppet/manifests/txgh_default.pp
@@ -7,3 +7,19 @@ include bootstrap
 include orcid_base::baseapps
 include orcid_base::common_libs
 include orcid_txgh
+
+user { "orcid_txgh":
+  ensure  => present,
+  uid  => '7012',
+  shell  => '/bin/bash',
+  home  => '/home/orcid_txgh',
+  managehome => true,
+  password => 'd9hliCDStS+VyAmWAZuCu4xnSRDsHIEE/Ve3/BzcToxp4tJDMxcn+CoJxtQl2+MZS1Lhp+jTLtqviDP1NbqpX2IOE9xapZHVUi8AWOjD',
+}
+
+file { "/home/orcid_txgh":
+  ensure  => directory,
+  owner => orcid_txgh,
+  group => orcid_txgh,
+  require => User["orcid_txgh"],
+}
--- puppet/modules/orcid_txgh/manifests/init.pp
+++ puppet/modules/orcid_txgh/manifests/init.pp
@@ -14,25 +14,27 @@ class orcid_txgh {
   exec { "ruby":
     environment => [ "DEBIAN_FRONTEND=noninteractive" ], # same as export DEBIAN_FRONTEND=noninteractive
     command => template("orcid_txgh/scripts/install_ruby.erb"),
-    require => Package["wget"]
+    require => Package["wget"],
+    timeout => 1800,
   }
 
-  $txgh_loc = '/home/$USER'
+  $txgh_loc = '/home/orcid_txgh'
   $txgh_rb = "txgh-master"
   $txgh_zip = "${txgh_rb}.zip"
 
 
   # download the txgh-master zip
-  file { "/home/$USER/$txgh_zip":
-    path   => "/home/$USER/$txgh_zip",
+  file { "/home/orcid_txgh/$txgh_zip":
+    path   => "$txgh_loc/$txgh_zip",
     source  => "puppet:///modules/orcid_txgh/$txgh_zip",
+    require => Exec["ruby"]
   }
 
   # unzip txgh-master zip at the desired location
   exec { "unzip $txgh_zip":
-    command => "unzip $txgh_loc/$txgh_zip",
-    creates => "/$txgh_loc/$txgh_rb",
-    require => Exec["ruby"]
+    command => "sudo unzip $txgh_loc/$txgh_zip -d $txgh_loc",
+    creates => "$txgh_loc/$txgh_rb",
+    require => File["/home/orcid_txgh/$txgh_zip"]
   }
 
   exec { "bundler":
@@ -42,22 +44,24 @@ class orcid_txgh {
     require => Exec["unzip $txgh_zip"]
   }
 
-  $ngrok_loc = '/home/$USER'
+  $ngrok_loc = '/home/orcid_txgh'
   $ngrok_bin = "ngrok-stable-linux-amd64"
   $ngrok_zip = "${ngrok_bin}.zip"
 
 
   # download the ngrok zip
-  file { "/home/$USER/$ngrok_zip":
-    path   => "/home/$USER/$ngrok_zip",
+  file { "/home/orcid_txgh/$ngrok_zip":
+    path   => "/home/orcid_txgh/$ngrok_zip",
     source  => "puppet:///modules/orcid_txgh/$ngrok_zip",
   }
 
   # unzip ngrok zip at the desired location
   exec { "unzip $ngrok_zip":
-    command => "unzip $ngrok_loc/$ngrok_zip",
+    command => "sudo unzip $ngrok_loc/$ngrok_zip",
     creates => "/$ngrok_loc/$ngrok_bin",
     require => Exec["bundler"]
   }
+
+
 
 }
