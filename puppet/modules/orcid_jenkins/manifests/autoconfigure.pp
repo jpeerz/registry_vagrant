77ed78e66372f686f223010badd4894eeb7fe190
Jeff <jeff@siccr.com>
2016-10-06 15:59:14 -0600

    update autoconfigure routine to user existin ssh key on server

--- puppet/modules/orcid_jenkins/manifests/autoconfigure.pp
+++ puppet/modules/orcid_jenkins/manifests/autoconfigure.pp
@@ -1,4 +1,5 @@
 class orcid_jenkins::autoconfigure {
+  
   $plugins = [
     "pam-auth",
     "plain-credentials",
@@ -33,11 +34,10 @@ class orcid_jenkins::autoconfigure {
   ]
   
   $jobs = [
-    "ORCID-push_checker"
+    "test"
   ]
-  $jenkins_port   = '8383'
-  $jenkins_user   = 'jenkins'
-  $jenkins_group  = 'jenkins'  
+  
+  # "ORCID-push_checker",
         
   exec { "download_jenkins_cli":
     onlyif   => "test ! -f /var/lib/jenkins/jenkins-cli.jar",
@@ -53,44 +53,7 @@ class orcid_jenkins::autoconfigure {
     group   => jenkins,
     require => Exec['download_jenkins_cli']
   }
-  
-  exec { "disable_jenkins_security":
-    require  => File["/var/lib/jenkins/"],
-    provider => 'shell',
-    path     => ["/bin","/usr/bin"],
-    cwd      => "/var/lib/jenkins/",
-    command  => "sed -i 's/FullControlOnceLoggedInAuthorizationStrategy/hudson.security.AuthorizationStrategy\$Unsecured/' /var/lib/jenkins/config.xml"
-  }
-  
-  # and use /modules/orcid_jenkins/files/var/lib/jenkins/bootstrap_config.xml <slaveAgentPort>49187</slaveAgentPort>
-  # to accept CLI calls
-  # create tmp user
-  # echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("test", "test1234")' |  java -jar /var/lib/jenkins/jenkins-cli.jar -s http://localhost:8383/ groovy =  
-    
-  file { "/etc/default/jenkins_config":
-    replace       => true,
-    ensure        => file,
-    path          => '/etc/default/jenkins_config',
-    content       => template('orcid_jenkins/etc/default/jenkins_config.erb'),
-    notify        => Exec['go_config_mode']
-  }
-
-  exec { "go_config_mode":
-    require  => Exec['disable_jenkins_security'],
-    provider => 'shell',
-    path     => ["/bin","/usr/bin"],    
-    command  => 'cp -Rfa /etc/default/jenkins /etc/default/jenkins_prod && \cp -Rfa /etc/default/jenkins_config /etc/default/jenkins',
-    notify   => Service['jenkins']
-  }
       
-  orcid_jenkins::job { "test": }  
-  
-  exec { "enable_jenkins_security":
-    provider => 'shell',
-    path     => ["/bin","/usr/bin"],
-    cwd      => "/var/lib/jenkins/",
-    command  => "sed -i 's/hudson.security.AuthorizationStrategy\$Unsecured/FullControlOnceLoggedInAuthorizationStrategy/' /var/lib/jenkins/config.xml"
-  }
-  
+  orcid_jenkins::job { $jobs:}
 
 }
--- puppet/modules/orcid_jenkins/manifests/job.pp
+++ puppet/modules/orcid_jenkins/manifests/job.pp
@@ -1,25 +1,29 @@
 # java -jar ~/bin/jenkins-cli.jar -s http://192.168.25.37:8090/ -i ~/.ssh/id_rsa install-plugin create-job NAME < xml
 define orcid_jenkins::job(
+  $security    = "-i /var/lib/jenkins/.ssh/id_rsa_jenkins",
   $jenkins_cli = "java -jar /var/lib/jenkins/jenkins-cli.jar -s http://localhost:8383/",
-  $security = ""
+  $job_path    = "/var/lib/jenkins/jobs"
 ) {
-  # /var/lib/jenkins/jobs/ORCID-push_checker/
-  $job_path = "/var/lib/jenkins/jobs"
-  
-  file { "/tmp/$title.xml":
-    ensure => present
+
+  file { "/tmp/${title}.xml":
+    ensure  => file,
+    owner   => jenkins,
+    group   => jenkins,    
+    source  => "puppet:///modules/orcid_jenkins/var/lib/jenkins/jobs/${title}.xml"
   }
   
-  $create_job_command = "$jenkins_cli create-job $title < /tmp/$title.xml"
+  $create_job_command = "$jenkins_cli $security create-job $title < /tmp/${title}.xml"
   
   notify { "Running: $create_job_command":}
   
   exec { "${title}_job":
-    onlyif   => "test ! -d $job_path",
+    require  => [
+      File["/tmp/${title}.xml"]
+    ],
+    onlyif   => "test ! -d $job_path/$title && test -f /var/lib/jenkins/jenkins-cli.jar && test -f /var/lib/jenkins/.ssh/id_rsa_jenkins",
     provider => 'shell',
     path     => ["/bin","/usr/bin"],
     cwd      => "/var/lib/jenkins/",
-    command  => $create_job_command,
-    #require  => Service['jenkins']
+    command  => $create_job_command
   }
 }
