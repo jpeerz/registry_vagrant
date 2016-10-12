2e1b28206e968f0f0e3a1671617fe9639de412f8
Jeff <jeff@siccr.com>
2016-10-12 15:31:40 -0600

    include configuration content as parameter, using defaults/minimal

--- puppet/manifests/orcid_db.pp
+++ puppet/manifests/orcid_db.pp
@@ -2,4 +2,41 @@ Exec {
   path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
 }
 
-include orcid_base::postgresql_95
+class {
+  orcid_base::postgresql:
+  
+    version => "9.5",
+    
+    hba     => "
+local   all             postgres                                peer
+local   all             all                                     peer
+host    all             all             127.0.0.1/32            md5
+host    all             all             ::1/128                 md5
+    ",
+    
+    conf    => "
+data_directory             = '/var/lib/postgresql/9.5/main'
+hba_file                   = '/etc/postgresql/9.5/main/pg_hba.conf'
+ident_file                 = '/etc/postgresql/9.5/main/pg_ident.conf'
+external_pid_file          = '/var/run/postgresql/9.5-main.pid'
+port                       = 5432
+max_connections            = 100
+unix_socket_directories    = '/var/run/postgresql'
+ssl                        = true
+ssl_cert_file              = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+ssl_key_file               = '/etc/ssl/private/ssl-cert-snakeoil.key'
+shared_buffers             = 128MB
+dynamic_shared_memory_type = posix
+log_line_prefix            = '%t [%p-%l] %q%u@%d '
+log_timezone               = 'UTC'
+cluster_name               = '9.5/main'
+stats_temp_directory       = '/var/run/postgresql/9.5-main.pg_stat_tmp'
+datestyle                  = 'iso, mdy'
+timezone                   = 'UTC'
+lc_messages                = 'en_US.UTF-8'
+lc_monetary                = 'en_US.UTF-8'
+lc_numeric                 = 'en_US.UTF-8'
+lc_time                    = 'en_US.UTF-8'
+default_text_search_config = 'pg_catalog.english'    
+    "  
+}
--- puppet/modules/orcid_base/manifests/postgresql.pp
+++ puppet/modules/orcid_base/manifests/postgresql.pp
@@ -1,6 +1,10 @@
-class orcid_base::postgresql_95 {
+class orcid_base::postgresql(
+  $version = "9.5",
+  $hba,
+  $conf
+) {
   
-  $pg_package_name = "postgresql-9.5"  
+  $pg_package_name = "postgresql-${version}"
   
   exec { "pg_install_apt_key":
     onlyif  => "test $(apt-key list | grep postgresql | wc -l) -eq 0",
@@ -14,7 +18,7 @@ class orcid_base::postgresql_95 {
     require => Exec['pg_install_apt_key']
   }
   
-  exec { "pg_upgate_codename":
+  exec { "pg_update_codename":
     onlyif  => "test $(grep $(lsb_release -cs) /etc/apt/sources.list.d/pgdg.list | wc -l) -eq 0",
     command => "sed -i s/DISTROCODENAME/$(lsb_release -cs)/g /etc/apt/sources.list.d/pgdg.list",
     require => File["/etc/apt/sources.list.d/pgdg.list"]
@@ -22,13 +26,31 @@ class orcid_base::postgresql_95 {
   
   exec { "pg_upgate_ubuntu_packages_db":
     creates => "/usr/bin/psql",
-    command => "sudo apt-get update 2>&1 > /dev/null",
-    require => Exec['pg_upgate_codename']
+    command => "apt-get update 2>&1 > /dev/null",
+    require => Exec['pg_update_codename']
   }
   
   package{$pg_package_name: 
     ensure  => installed,
     require => Exec['pg_upgate_ubuntu_packages_db']
   }
+  
+  file { "/etc/postgresql/$version/main/pg_hba.conf":
+    ensure  => file,
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '0440',
+    content  => $hba,
+    require => Package[$pg_package_name],
+  }
+
+  file { "/etc/postgresql/$version/main/postgresql.conf":
+    ensure  => file,
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '0440',
+    content  => $conf,
+    require => Package[$pg_package_name],
+  }  
 
 }
